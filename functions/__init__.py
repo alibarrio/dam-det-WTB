9d2cdb80f3cca9b6ebc337a8fb9b49ab8471c6c2
alibarrio <102653986+alibarrio@users.noreply.github.com>
2022-04-22 12:49:10 +0200

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,2 @@
-from .pair_dataset import siamese_dataset
 from .siamese_resnet import SiameseInceptionResnetV1, Siamese
-from .net_train import pass_epoch
 from .create_dataset import createTrain, createTest, splitDataSet
