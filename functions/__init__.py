5a40859df62801e7c1a5b583e074e0012934dc1b
alibarrio <102653986+alibarrio@users.noreply.github.com>
2022-04-22 12:32:23 +0200

    name change

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,4 @@
 from .pair_dataset import siamese_dataset
 from .siamese_resnet import SiameseInceptionResnetV1, Siamese
 from .net_train import pass_epoch
-from .croppedYaleddbb import croppedYaleTrain, croppedYaleTest, splitDataSet
+from .create_dataset import create_Train, create_Test, splitDataSet
