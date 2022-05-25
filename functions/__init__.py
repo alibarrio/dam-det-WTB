d89583fc45765c776ffe248f95001da73a5f41fc
alibarrio <102653986+alibarrio@users.noreply.github.com>
2022-05-25 11:49:27 +0200

    Delete functions directory

--- functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from .siamese_resnet import Siamese
-from .create_dataset import createTrain, createTest, splitDataSet
--- functions/utils.py
+++
@@ -1,4 +0,0 @@
-
-def fixed_image_standardization(image_tensor):
-    processed_tensor = (image_tensor - 127.5) / 128.0
-    return processed_tensor
